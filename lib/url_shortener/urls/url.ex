34d8c2ace448ab18b7f2c1d42b6591eca21dba8d
Berkan Yavri <yavrib@gmail.com>
2024-09-03 16:31:45 +0200

    Remove comments

--- lib/url_shortener/urls/url.ex
+++ lib/url_shortener/urls/url.ex
@@ -1,7 +1,7 @@
 defmodule UrlShortener.Urls.Url do
   use Ecto.Schema
   import Ecto.Changeset
-  import EctoCommons.URLValidator # <- here
+  import EctoCommons.URLValidator
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -19,6 +19,6 @@ defmodule UrlShortener.Urls.Url do
     url
     |> cast(attrs, [:id, :destination, :path, :visited, :user_id])
     |> validate_required([:id, :destination, :path, :visited, :user_id])
-    |> validate_url(:destination) # <- here
+    |> validate_url(:destination)
   end
 end
